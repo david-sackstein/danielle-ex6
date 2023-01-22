bd90cd36e331a9f74a3d4d68136099c63e434fe1
David Sackstein <davids@codeprecise.com>
2023-01-23 00:05:52 +0200

    Step 13

--- src/oop/ex6/strings/Literals.java
+++ src/oop/ex6/strings/Literals.java
@@ -4,8 +4,9 @@ import static oop.ex6.strings.Primitives.*;
 
 public class Literals {
     public static final String INT = "-?" + WHITESPACES + DIGIT + "+";
+    // TODO: check whether DOUBLE covers first digit zero or starting with a dot.
     public static final String DOUBLE = "(-?[1-9]+\\d*(\\.[0-9])*[0-9]*|0(\\.[0-9])*[0-9]*)";
     public static final String CHAR = "'.'";
-    public static final String STRING = "\"" + WHITESPACES + ".*" + WHITESPACES + "\"";
+    public static final String STRING = "\".*\"";
     public static final String BOOLEAN = "true|false|" + DOUBLE + "|" + INT;
 }
